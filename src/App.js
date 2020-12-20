2de16605ba34bce31f9b9b76f815b24d808567a6
Arun-Tiwary <70338393+Arun-Tiwary@users.noreply.github.com>
2020-12-20 19:19:06 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
+var action = ["Fast & Furious", "Misson Impossible", "6 Underground", "Venom"];
+
+var drama = ["The Social Delima", "Ford vs Ferrari", "Parasite", "Lion"];
+
+var thriller = ["Joker", "Gone Girl", "Nightcrawler"];
+
 export default function App() {
+  const [country, setCountry] = useState("");
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2> {country}</h2>
+      <button
+        onClick={() =>
+          setCountry(
+            action.map(function (item) {
+              return item;
+            })
+          )
+        }
+      ></button>
     </div>
   );
 }
